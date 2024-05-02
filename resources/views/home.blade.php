1584083e1519372f94a73c57f83b6dd8d5fc5cf6
elshodatc111 <elshodatc1116@gmail.com>
2024-05-02 22:53:52 +0300

    Save

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,50 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">O'quv markazlar</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <table class="table table-bordered text-center">
+                        <tr>
+                            <th>#</th>
+                            <th>O'quv Markaz</th>
+                            <th>Drektor</th>
+                            <th>Telefon</th>
+                            <th>Status</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-12">
+            <div class="card mt-3">
+                <div class="card-header">Yangi o'quv markazlar</div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <label for="">O'quv markaz nomi</label>
+                                <input type="text" required class="form-control">
+                            </div>
+                            <div class="col-lg-6">
+                                <label for="">O'quv markaz drektori</label>
+                                <input type="text" required class="form-control">
+                            </div>
+                            <div class="col-lg-6">
+                                <label for="">Sayt manzili</label>
+                                <input type="text" required class="form-control">
+                            </div>
+                            <div class="col-lg-6">
+                                <label for="">O'quv markaz telefon raqami</label>
+                                <input type="text" required class="form-control">
+                            </div>
+                            <div class="col-12 text-center">
+                                <button class="btn btn-primary w-50 mt-2">O'quv markazni saqlash</button>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </form>
                 </div>
             </div>
         </div>
